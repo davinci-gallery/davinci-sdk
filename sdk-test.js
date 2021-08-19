3afa425890a882303fedc8b930dce06b5f209b82
kuyawa <haxapp@gmail.com>
2021-08-19 16:31:44 -0400

    DaVinci SDK

--- sdk-test.js
+++ sdk-test.js
@@ -3,19 +3,19 @@ let sdk = new SDK(process.env.CHAINID); // 1.mainnet 2.testnet
 
 
 async function main() {
-	sdk.newWallet(process.env.PRIVATEKEY);  // Your private key
-	console.log('Wallet', sdk.wallet);
+    sdk.newWallet(process.env.PRIVATEKEY);  // Your private key
+    console.log('Wallet', sdk.wallet);
 
-	// To create an NFT
-	let data = {
-		name       : 'My first token',
-		description: 'This is a cool token freshly minted',
-		saleprice  : 10.00,
-		file       : './anyimg.jpg',   // absolute or relative path to image
-	};
+    // To create an NFT
+    let data = {
+        name       : 'My first token',
+        description: 'This is a cool token freshly minted',
+        saleprice  : 10.00,
+        file       : './anyimg.jpg',   // absolute or relative path to image
+    };
 
-	let info = await sdk.createNFT(data);
-	console.log('New token', info);
+    let info = await sdk.createNFT(data);
+    console.log('New token', info);
 }
 
 main();
